31b6eb8608a6b4ad3c6a578dfaa47221395255ba
rlaekdud <gloria030114@gmail.com>
2024-01-20 16:45:35 +0900

    Feat: GameService.determineWinner() Impl
    - 승자를 List<RacingCar>로 반환하는 determineWinner() 구현
    - Collectors를 사용해 구현하려고 노력했음
    - GameService를 처음 만들 때 racingCarNameList를 받아서 initialize할 수 있도록 수정

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -3,14 +3,20 @@ package racingcar.service;
 import racingcar.model.RacingCar;
 import racingcar.utils.RandomNumberGenerator;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GameService {
 
     private List<RacingCar> racingCarList;
 
-    public GameService(List<RacingCar> racingCarList) {
-        this.racingCarList = racingCarList;
+    public GameService(List<String> racingCarNameList) { // 처음 생성 시 init
+        this.racingCarList = new ArrayList<>();
+
+        for(String racingCarName : racingCarNameList) {
+            racingCarList.add(new RacingCar(racingCarName));
+        }
     }
 
     public void playOneRound() { // 한 라운드를 진행, 출력은 하지 않음(출력은 controller에서!)
@@ -18,6 +24,17 @@ public class GameService {
         for(RacingCar racingCar : racingCarList) {
             if(isAdvance()) racingCar.advance();
         }
+    }
+
+    public List<RacingCar> determineWinner() {
+        int maxDistance = racingCarList.stream()
+                .mapToInt(RacingCar::getDistance)
+                .max()
+                .orElseThrow(IllegalArgumentException::new);
+
+        return racingCarList.stream()
+                .filter(car -> car.getDistance() == maxDistance)
+                .collect(Collectors.toList());
     }
 
     private boolean isAdvance() {
